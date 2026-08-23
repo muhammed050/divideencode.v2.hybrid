a501e125b32e7b494856b2341980b7e35cac9d43
muhammed050 <73080022+muhammed050@users.noreply.github.com>
2026-08-24 00:30:33 +0300

    feat: expose universal binary IR

--- divideencode/__init__.py
+++ divideencode/__init__.py
@@ -2,6 +2,9 @@ from .encoder import compress, recursive_compress, MAGIC as CONTAINER_MAGIC, VER
 from .decoder import decompress, decompress_with_trace, parse_header
 from .universal import compress as universal_compress, decompress as universal_decompress
 from .adaptive_universal import compress as adaptive_compress, decompress as adaptive_decompress
+from .universal_ir import Kind as UniversalIRKind, Candidate as UniversalIRCandidate
+from .universal_ir import transform as universal_ir_transform, inverse as universal_ir_inverse
+from .universal_ir import rank as universal_ir_rank, best as universal_ir_best
 from .errors import DivideEncodeError, CorruptedError, NotDivideEncodedError
 from .strategies import render_trace
 
@@ -14,6 +17,12 @@ __all__ = [
     "universal_decompress",
     "adaptive_compress",
     "adaptive_decompress",
+    "UniversalIRKind",
+    "UniversalIRCandidate",
+    "universal_ir_transform",
+    "universal_ir_inverse",
+    "universal_ir_rank",
+    "universal_ir_best",
     "decompress_with_trace",
     "parse_header",
     "recursive_compress",
